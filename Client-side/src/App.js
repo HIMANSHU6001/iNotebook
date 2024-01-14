f01ba9e4bd919cf317511a98217ae44195693ecf
Himanshu6001 <hk9797592893@gmail.com>
2024-01-14 12:24:13 +0530

    all set

---
+++ Client-side/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import { Navbar } from './Components/Navbar';
+import { Home } from './Components/Home';
+import { About } from './Components/About';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path='/about' element={<About/>}/>
+        <Route exact path='/' element={<Home/>}/> 
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
