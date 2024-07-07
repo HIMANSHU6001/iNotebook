35153764cd771933ae63e72c0248b82a359f0bd8
HIMANSHU6001 <hk9797592893@gmail.com>
2024-07-07 15:06:55 +0530

    created seprate backend and frontend

---
+++ client-side/src/components/Alert.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+function Alert(props) {
+  const { alert, closeAlert } = props;
+
+  const getColor = () => {
+    if (alert.type === "success") {
+      return "bg-green-50 text-green-700"
+    }
+    else if (alert.type === "danger") {
+      return "bg-red-50 text-red-700"
+    }
+    else if (alert.type === "warning") {
+      return "bg-yellow-50 text-yellow-700"
+    }
+    else if (alert.type === "info") {
+      return "bg-blue-50 text-blue-700"
+    }
+  }
+
+  return (
+    <>
+      {alert && <div class={`fixed z-30 w-full flex items-center p-4 mb-4 ${getColor()}`} role="alert">
+        <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+        </svg>
+        <span class="sr-only">Close</span>
+        <div class="ms-3 text-sm font-medium">
+          <span className='mx-5'>{alert.type === "success" && <span class="text-green-700">Success</span>}
+            {alert.type === "danger" && <span class="text-red-700">Error</span>}
+            {alert.type === "warning" && <span class="text-yellow-700">Warning</span>}
+            {alert.type === "info" && <span class="text-blue-700">Info</span>}
+          : </span>
+          {alert.msg}
+        </div>
+        <button onClick={() => { closeAlert() }} type="button" class="ms-auto -mx-1.5 -my-1.5 px-4" data-dismiss-target="#alert-1" aria-label="Close">
+          <span class="sr-only">Close</span>
+          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+          </svg>
+        </button>
+      </div>}
+    </>
+  )
+}
+
+export default Alert
